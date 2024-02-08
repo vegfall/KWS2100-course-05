0aac9c2de4a613c184293d6c084b74587e77f518
Vegard Falldalen <vegfall@gmail.com>
2024-02-08 10:56:40 +0100

    Placeholder for school layer

--- src/modules/application/application.tsx
+++ src/modules/application/application.tsx
@@ -9,6 +9,8 @@ import { Layer } from "ol/layer";
 import { KommuneAside } from "../kommune/kommuneAside";
 import { FylkeLayerCheckbox } from "../fylke/fylkeLayerCheckbox";
 import { FylkeAside } from "../fylke/fylkeAside";
+import { SchoolLayerCheckbox } from "../school/schoolLayerCheckbox";
+import { SchoolAside } from "../school/schoolAside";
 
 export function Application() {
   const mapRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -48,11 +50,13 @@ export function Application() {
         </a>
         <KommuneLayerCheckbox />
         <FylkeLayerCheckbox />
+        <SchoolLayerCheckbox />
       </nav>
       <main>
         <div ref={mapRef}></div>
         <KommuneAside />
         <FylkeAside />
+        <SchoolAside />
       </main>
     </MapContext.Provider>
   );
---
+++ src/modules/school/schoolLayerCheckbox.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function SchoolLayerCheckbox() {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" />
+        Schools out
+      </label>
+    </div>
+  );
+}
