d9306f759ee3ccf030c2286eceb65a5b9f0f7872
Vegard Falldalen <vegfall@gmail.com>
2024-02-08 11:47:51 +0100

    Added custom style for school

--- src/modules/school/schoolLayerCheckbox.tsx
+++ src/modules/school/schoolLayerCheckbox.tsx
@@ -3,6 +3,7 @@ import VectorLayer from "ol/layer/Vector";
 import VectorSource from "ol/source/Vector";
 import { GeoJSON } from "ol/format";
 import { useLayer } from "../map/useLayer";
+import { Fill, Stroke, Style, Circle } from "ol/style";
 
 const schoolLayer = new VectorLayer({
   className: "schools",
@@ -10,10 +11,17 @@ const schoolLayer = new VectorLayer({
     url: "schools.json",
     format: new GeoJSON(),
   }),
+  style: new Style({
+    image: new Circle({
+      stroke: new Stroke({ color: "blue", width: 2 }),
+      fill: new Fill({ color: "white" }),
+      radius: 4,
+    }),
+  }),
 });
 
 export function SchoolLayerCheckbox() {
-  const [checked, setChecked] = useState(false);
+  const [checked, setChecked] = useState(true);
 
   useLayer(schoolLayer, checked);
 
